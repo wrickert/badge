335c66323f33a03e5c8e7123cb513fd6422c1e19
woolery <woolery@iastate.edu>
2019-04-15 17:30:01 -0500

    I can now play the original theme from the Legend of Zelda: Links Awakening
    The

--- Firmware/main.py
+++ Firmware/main.py
@@ -3,60 +3,93 @@ import time
 from notes import *
 from math import ceil
 
+
+def play_zelda():
+    """
+    Play the main theme from Link's awakening.
+    :return:
+    """
+    rainbowPiano.play_note(AS5, .314)
+    time.sleep(.091)
+    rainbowPiano.play_note(F5, .522)
+    rainbowPiano.play_note(AS5)
+    rainbowPiano.play_note(AS5)
+    rainbowPiano.play_note(C6)
+    rainbowPiano.play_note(D6)
+    rainbowPiano.play_note(DS6)
+    # 1.19
+    rainbowPiano.play_note(F6, .506)
+    time.sleep(.507)
+    rainbowPiano.play_note(F6)
+    time.sleep(.1)
+    rainbowPiano.play_note(F6)
+    time.sleep(.1)
+    rainbowPiano.play_note(FS6)
+    rainbowPiano.play_note(GS6)
+    # 3.07
+    rainbowPiano.play_note(AS6, .517)
+    # Top of the first swell
+
+    rainbowPiano.play_note(AS6)
+    time.sleep(.1)
+    rainbowPiano.play_note(AS6)
+    time.sleep(.1)
+    rainbowPiano.play_note(GS6)
+    rainbowPiano.play_note(FS6)
+    # 4.26
+    rainbowPiano.play_note(GS6, .304)
+    rainbowPiano.play_note(FS6)
+    rainbowPiano.play_note(F6, .507)
+    time.sleep(.241)
+    rainbowPiano.play_note(F6, .304)
+    time.sleep(.1)
+    # 6.15
+    rainbowPiano.play_note(DS6, .315)
+    rainbowPiano.play_note(F6)
+    rainbowPiano.play_note(FS6, .517)
+    time.sleep(.304)
+    rainbowPiano.play_note(F6)
+    time.sleep(.1)
+    rainbowPiano.play_note(DS6)
+    time.sleep(.1)
+    # 8.03
+    rainbowPiano.play_note(CS6)
+    rainbowPiano.play_note(DS6)
+    rainbowPiano.play_note(F6, .506)
+    time.sleep(.304)
+    rainbowPiano.play_note(DS6)
+    time.sleep(.1)
+    rainbowPiano.play_note(CS6)
+    time.sleep(.1)
+    # 9.22
+    rainbowPiano.play_note(C6, .304)
+    rainbowPiano.play_note(D6)
+    rainbowPiano.play_note(E6, .497)
+    time.sleep(.304)
+    rainbowPiano.play_note(G6, .314)
+    time.sleep(.1)
+    # 11.11
+    rainbowPiano.play_note(F6)
+    time.sleep(.1)
+    rainbowPiano.play_note(ceil((AS5 + F5) / 2))
+    time.sleep(.001)
+    rainbowPiano.play_note(ceil((AS5 + F5) / 2))
+    time.sleep(.001)
+    rainbowPiano.play_note(ceil((AS5 + F5) / 2))
+    time.sleep(.1)
+    rainbowPiano.play_note(ceil((AS5 + F5) / 2))
+    time.sleep(.001)
+    rainbowPiano.play_note(ceil((AS5 + F5) / 2))
+    time.sleep(.001)
+    rainbowPiano.play_note(ceil((AS5 + F5) / 2))
+
+    rainbowPiano.clear()
+
+
 # POST stuff for testing
 f = open('silent.txt')
 if f.read() == 'f':
-
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(A4)
-
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(AS4)
-
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(A4)
-
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(CS4)
-
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(A4)
-
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(ceil((CS4 + CS5)/2), 1)
-
-    rainbowPiano.play_note(ceil((CS5 + A4 + D4)/3), 1)
-
-    rainbowPiano.play_note(D4, 1)
-    rainbowPiano.play_note(C5, 1)
-    rainbowPiano.play_note(C4, 1)
-
-    # Second stave
-
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(A4)
-
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(AS4)
-
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(C5)
-
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(D4)
-    rainbowPiano.play_note(ceil((CS5 + A3)/2))
-
-    rainbowPiano.play_note(ceil((CS4 + A4 + FS4 + D4)/4), 1)
-    rainbowPiano.play_note(ceil((D6 + FS5 + D5 + D4)/4), 1.5)
+    play_zelda()
 
     rainbowPiano.clear()
 
@@ -67,3 +100,4 @@ else:
 
 f.close()
 rainbowPiano.keys()
+
--- Firmware/rainbowPiano.py
+++ Firmware/rainbowPiano.py
@@ -2,6 +2,7 @@ import machine
 import time
 import neopixel
 from notes import *
+import random
 
 import esp32
 
@@ -95,25 +96,25 @@ def setTone(key):
         stackColor(1)
         print("1")
         speaker.duty(100)
-        speaker.freq(440)
+        speaker.freq(G4)
         current = 1
     elif key == 2 and current != key:
         stackColor(2)
         print("2")
         speaker.duty(100)
-        speaker.freq(494)
+        speaker.freq(B4)
         current = 2
     elif key == 3 and current != key:
         stackColor(3)
         print("3")
         speaker.duty(100)
-        speaker.freq(554)
+        speaker.freq(CS5)
         current = 3
     elif key == 4 and current != key:
         stackColor(4)
         print("4")
         speaker.duty(100)
-        speaker.freq(587)
+        speaker.freq(D5)
         current = 4
 
 def clear():
@@ -124,8 +125,15 @@ def clear():
         time.sleep(0.02)
 
 
-def play_note(freq: int, play_time: float = 0.5):
+def play_note(freq: int, play_time: float = .2):
+    """
+    Plays a note of frequency `freq` for duration `play_time`, and lights up a random LED for fun
+    :param freq: The frequency of the note to play, in hz
+    :param play_time: The duration of the note, in seconds
+    :return: Null
+    """
     speaker.duty(100)
     speaker.freq(freq)
+    stackColor(random.randint(1, 4))
     time.sleep(play_time)
     speaker.duty(0)
